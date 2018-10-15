33844a15538b0e4504b33ed62b761c0cae3ea9ae
chsyisgood <chsy973414741@gmail.com>
2018-10-15 16:21:20 -0400

    111

--- SolarSystem/src/Body.java
+++ SolarSystem/src/Body.java
@@ -1,11 +1,11 @@
 
 public class Body {
-    private char name;
+    private String name;
     private double mass;
     private double diameter;
     private Body orbits;
     private double meandistance;
-    public Body(char name, double mass, double diameter, Body orbits, double meandistance) {
+    public Body(String name, double mass, double diameter, Body orbits, double meandistance) {
         this.name = name;
         this.mass = mass;
         this.diameter = diameter;
