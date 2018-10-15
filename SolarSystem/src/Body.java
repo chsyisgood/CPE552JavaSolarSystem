a8e23392fa559848c063fe3186f756afc8624d03
Ninesss <623840239@qq.com>
2018-10-15 16:14:19 -0400

    body

--- SolarSystem/src/Body.java
+++ SolarSystem/src/Body.java
@@ -1,7 +1,17 @@
 
 public class Body {
-    private Body* orbits;
+    private char name;
     private double mass;
+    private double diameter;
+    private Body orbits;
+    private double meandistance;
+    public Body(char nm, double mas, double dia, Body orbit, double distance) {
+        name = nm;
+        mass = mas;
+        diameter = dia;
+        orbits = orbit;
+        meandistance = distance;
+    }
     /*public static void main(String[] args) {
         System.out.println("Hello World!");
     }*/
